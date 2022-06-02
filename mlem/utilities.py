5ec889b9502ae17636484e05ee01af4c8611422f
Gerardo Zinno <g.zinno@studenti.unipi.it>
2022-06-02 16:29:52 +0200

    Add functions to generate noise

--- mlem/utilities.py
+++ mlem/utilities.py
@@ -1,6 +1,6 @@
 import bz2
 import pickle
-from typing import Any, Dict, List, Tuple, Union, Iterable
+from typing import Any, Dict, List, Tuple, Union, Iterable, Callable
 import numpy as np
 from numpy import ndarray, concatenate
 from pandas import concat
@@ -14,6 +14,7 @@ from numpy.lib.arraysetops import unique
 from sklearn.utils import resample
 from sklearn.experimental import enable_halving_search_cv
 from sklearn.model_selection import HalvingGridSearchCV
+import pandas as pd
 
 
 def save_pickle_bz2(path: str, object: Any):
@@ -233,3 +234,75 @@ def frequencies(values: Union[Iterable, int, float]) -> np.array:
     # unique, counts
     u, c = np.unique(values, return_counts=True)
     return np.asarray((u, c)).T
+
+
+def frequency_based_noise(column, size):
+    """
+    Sample values from a column with replacement.
+
+    Args:
+        column: column to sample from
+        size: number of elements to sample
+
+    Returns:
+        Array of samples
+    """
+    return column.sample(size, replace=True).to_numpy()
+
+
+def insert_noise_categorical(dataset: pd.DataFrame, perc: float = 0.1,
+                             noise_generating_function: Callable[[pd.Series, int], np.array] = frequency_based_noise):
+    """
+    Insert noise in a categorical dataset and returns the dataset passed as argument.
+
+    Args:
+        dataset (DataFrame): dataset on which to insert the noise ( it should only contain categorical variables )
+        perc (float): percentage of noise in the range [0,1]
+       noise_generating_function (Callable[[int], array]): function used to generate the noise, must take as input the number of noisy values to
+                                   generate inside an argument named size and return an array containing the random values.
+
+    Returns:
+        dataset
+    """
+    n_rows, n_col = dataset.shape
+    percentage = int(perc * n_rows)
+
+    for c in range(n_col):
+        index_to_replace = np.random.choice(dataset.index,
+                                            size=percentage)
+        new_values = noise_generating_function(dataset[dataset.columns[c]], size=percentage)
+        assert (len(index_to_replace) == len(new_values))
+        for ind, val in zip(index_to_replace, new_values):
+            dataset.iloc[ind, c] = val
+    return dataset
+
+
+def insert_noise_numerical(dataset: DataFrame, perc: float = 0.1,
+                           noise_generating_function: Callable[[int], np.array] = np.random.normal):
+    """
+    Insert noise in a numerical dataset and returns the dataset passed as argument.
+
+    Args:
+        dataset (DataFrame): dataset on which to insert the noise.
+        perc (float): percentage of noise in the range [0,1]
+        noise_generating_function (Callable[[int], array]): function used to generate the noise, must take as input the number of noisy values to
+                                   generate inside an argument named size and return an array containing the random values.
+
+
+    Examples:
+
+        >>> df = pd.DataFrame(data={'col1': 10 * [1], 'col2': 10 * [2], 'col3': 10 * [3]})
+        >>> df[NUMERICAL] = insert_noise_numerical(df[NUMERICAL].copy(), perc=0.1, noise_generating_function=np.random.rand) # note np.random.rand has a size parameter
+
+    """
+    n_rows, n_col = dataset.shape
+    percentage = int(perc * n_rows)
+
+    for c in range(n_col):
+        index_to_replace = np.random.choice(dataset.index,
+                                            size=percentage)
+        new_values = noise_generating_function(size=percentage)
+        assert (len(index_to_replace) == len(new_values))
+        for ind, val in zip(index_to_replace, new_values):
+            dataset.iloc[ind, c] = val
+    return dataset
