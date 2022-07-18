3e2c77f438b1de094c41b7beaa20c820ef048442
Gerardo Zinno <g.zinno@studenti.unipi.it>
2022-07-19 00:29:20 +0200

    Add source

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 cd ..
-echo "Going to $PWD"
+echo "Going to: $PWD"
+
+echo "Sourcing virtual env"
+source venv/bin/activate
 
 echo "Running experiments on Adult with Adaboost"
 time ./adult_randfor_clustered_adaboost.py rf notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/adult_rf_noclustering.bz2 notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/adult_rf_noclustering_data_nsamples_2.npz --results-path "adult_results_shadow_nsamples_2_06_train" --local-attack-dataset-path notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/validation_noisy.csv
