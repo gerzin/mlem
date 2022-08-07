0687c9adbd726042dd173ab4bc845bcf6efd0279
Gerardo Zinno <g.zinno@studenti.unipi.it>
2022-08-07 20:47:35 +0200

    Exp. no oversampling

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -6,7 +6,7 @@ echo "Sourcing virtual env"
 source venv/bin/activate
 
 echo "Running experiments on Adult with Adaboost"
-time ./adult_randfor_clustered_adaboost.py rf notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/adult_rf_noclustering.bz2 notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/adult_rf_noclustering_data_nsamples_2.npz --results-path "ADULT_TEST_CENTROIDS" --local-attack-dataset-path notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/validation_noisy.csv
+time ./adult_randfor_clustered_adaboost.py rf notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/adult_rf_noclustering.bz2 notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/adult_rf_noclustering_data_nsamples_2.npz --results-path "ADULT_TEST_CENTROIDS_NO_OVERSAMPLING_AFTER_PICKING" --local-attack-dataset-path notebooks/datasets/adult/BB_NO_CLUSTERING/BB_DATA/validation_noisy.csv
 
 echo "Running experiments on Diva with Adaboost"
-time ./diva_randfor_clustered_adaboost.py rf notebooks/datasets/diva/BB_NO_CLUSTERING/BB_DATA/diva_rf_noclustering.bz2 notebooks/datasets/diva/BB_NO_CLUSTERING/BB_DATA/diva_rf_noclustering_data_nsamples_5.npz --results-path "ADULT_TEST_CENTROIDS" --local-attack-dataset-path notebooks/datasets/diva/BB_NO_CLUSTERING/BB_DATA/validation_noisy.csv
+time ./diva_randfor_clustered_adaboost.py rf notebooks/datasets/diva/BB_NO_CLUSTERING/BB_DATA/diva_rf_noclustering.bz2 notebooks/datasets/diva/BB_NO_CLUSTERING/BB_DATA/diva_rf_noclustering_data_nsamples_5.npz --results-path "DIVA_TEST_CENTROIDS_NO_OVERSAMPLING_AFTER_PICKING" --local-attack-dataset-path notebooks/datasets/diva/BB_NO_CLUSTERING/BB_DATA/validation_noisy.csv
